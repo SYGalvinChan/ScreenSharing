a65adc2e7316a7e2a77bacc7748fae84b695aadf
Galnerd <e0543848@u.nus.edu>
2024-01-10 19:49:19 +0800

    Add on click event to button

--- app/src/main/java/com/example/screensharing/MainActivity.java
+++ app/src/main/java/com/example/screensharing/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.screensharing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        EditText input_ip_address = (EditText) findViewById(R.id.editTextText);
+        EditText input_port_number = (EditText) findViewById(R.id.editTextNumber);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                input_port_number.setText(input_ip_address.getText());
+            }
+        });
     }
+
 }
